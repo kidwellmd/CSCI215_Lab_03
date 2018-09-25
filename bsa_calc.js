6024c6320b0a522b1bc95d10d886a86d3da72e13
kidwellmd <42657772+kidwellmd@users.noreply.github.com>
2018-09-25 18:01:42 -0400

    Add files via upload

---
+++ bsa_calc.js
@@ -0,0 +1,79 @@
+function calculate() {
+
+    var female = document.getElementsByName("sex")[1].checked;
+    var male = document.getElementsByName("sex")[0].checked;
+
+    var weight = document.getElementsByName("weight")[0].value;
+    var height = document.getElementsByName("height")[0].value;
+
+    console.log( 'Male = ' + male );
+    console.log( 'Weight = ' + weight );
+    console.log( 'Height = ' + height );
+
+    // -------------------------------
+    // Conversions
+    // -------------------------------
+    // lbs to kg: 1 lb = 0.454 kg
+    // inch to meter: 1 in = 0.0254 m
+    // inch to cm: 1 in = 2.54 cm
+
+    // ==========================================
+    // Todo: Perform conversion calculations here
+	var weight_kg = weight*.454;
+	var height_cm = height*2.54;
+	var height_m = height*.0254;
+	
+	
+    // -------------------------------
+    // Body Surface Area (BSA)
+    // -------------------------------
+
+	var BSA = 0;
+    
+	
+    // ==========================================
+    // Todo: Perform BSA calculation here
+	 BSA = math.sqrt(height_cm*weight_kg/3600);
+    // -------------------------------
+    // Ideal Body Weight (IBW)
+    // -------------------------------
+
+    var IBW = 0;
+	
+    if ( female ) {
+
+        // ==========================================
+        // Todo: Perform female IBW calculation here
+	IBW = 45.5+2.3*(height - 60);
+
+    } else if ( male ) {
+
+        // ==========================================
+        // Todo: Perform male IBW calculation here
+		IBW = 50+2.3*(height - 60)
+    }
+
+    // -------------------------------
+    // Body Mass Index (BMI)
+    // -------------------------------
+
+    var BMI = 0;
+    // ==========================================
+    // Todo: Perform BMI calculation here
+	BMI = weight_kg / math.pow(height_m, 2);
+
+
+    // Do not modify this function call
+    display( BSA, IBW, BMI );
+
+}
+
+// ------------------------------------
+// Do not modify this function
+function display( BSA, IBW, BMI ) {
+
+    var message = 'Body Surface Area = ' + BSA.toPrecision(4) + ' m^2\nIdeal Body Weight = ' + IBW.toPrecision(4) + ' kg\nBody Mass Index = ' + BMI.toPrecision(4) + ' kg/m^2';
+
+    document.getElementById("output").innerHTML=message;
+
+}
